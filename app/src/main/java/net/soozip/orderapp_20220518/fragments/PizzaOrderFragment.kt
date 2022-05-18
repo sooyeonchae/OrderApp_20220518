91d41ed697b24131d76c6087be8797037c012bcc
채수연 <sooyeonchae@gmail.com>
2022-05-18 17:22:35 +0900

    피자가게 데이터를 하드코딩으로 추가.

--- app/src/main/java/net/soozip/orderapp_20220518/fragments/PizzaOrderFragment.kt
+++ app/src/main/java/net/soozip/orderapp_20220518/fragments/PizzaOrderFragment.kt
@@ -6,8 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import net.soozip.orderapp_20220518.R
+import net.soozip.orderapp_20220518.datas.StoreData
 
 class PizzaOrderFragment : Fragment() {
+
+    val mPizzaStoreList = ArrayList<StoreData>()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,5 +24,9 @@ class PizzaOrderFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
+        mPizzaStoreList.add(StoreData("피자헛", "https://img1.daumcdn.net/thumb/R800x0/?scode=mtistory2&fname=https%3A%2F%2Fk.kakaocdn.net%2Fdn%2FnkQca%2FbtqwVT4rrZo%2FymhFqW9uRbzrmZTxUU1QC1%2Fimg.jpg", 3.5, "1588-5588"))
+        mPizzaStoreList.add(StoreData("파파존스", "http://mblogthumb2.phinf.naver.net/20160530_65/ppanppane_1464617766007O9b5u_PNG/%C6%C4%C6%C4%C1%B8%BD%BA_%C7%C7%C0%DA_%B7%CE%B0%ED_%284%29.png?type=w800", 4.5, "1588-5587"))
+        mPizzaStoreList.add(StoreData("도미노피자", "https://mblogthumb-phinf.pstatic.net/20160530_173/ppanppane_14646176567743d7hd_PNG/%B5%B5%B9%CC%B3%EB%C7%C7%C0%DA_%B7%CE%B0%ED_%282%29.png?type=w800", 1.0, "1588-5586"))
+        mPizzaStoreList.add(StoreData("미스터피자", "https://post-phinf.pstatic.net/MjAxODEyMDVfMzYg/MDAxNTQzOTYxOTA4NjM3.8gsStnhxz7eEc9zpt5nmSRZmI-Pzpl4NJvHYU-Dlgmcg.7Vpgk0lopJ5GoTav3CUDqmXi2-_67S5AXD0AGbbR6J4g.JPEG/IMG_1641.jpg?type=w1200", 2.5, "1577-0077"))
     }
 }
