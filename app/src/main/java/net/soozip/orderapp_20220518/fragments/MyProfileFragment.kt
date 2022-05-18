dbea0a02436e79bf6769751f2d780ac2d6d79ec4
채수연 <sooyeonchae@gmail.com>
2022-05-18 16:51:07 +0900

    tab layout과 뷰페이저를 배치하고, 프래그먼트 2개 생성.

---
+++ app/src/main/java/net/soozip/orderapp_20220518/fragments/MyProfileFragment.kt
@@ -0,0 +1,25 @@
+package net.soozip.orderapp_20220518.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import net.soozip.orderapp_20220518.R
+
+class MyProfileFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_my_profile, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        
+    }
+}
